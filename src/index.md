195a08c14027849773b84bb30a30c4a82d69a9d9
robothot <854363956@qq.com>
2022-01-05 21:30:53 +0800

    init: 初始化项目

---
+++ src/index.md
@@ -0,0 +1,52 @@
+---
+nav:
+    title: Components
+    path: /components
+group:
+    path: /components/print
+    title: 打印
+---
+
+## Table 表格
+
+```tsx
+/**
+ * title: 打印
+ * desc: 打印容器里面的内容
+ */
+import React, { useState, useRef } from 'react';
+import { Print } from '@weblif/rc-print';
+
+export default () => {
+    const print = useRef()
+    return (
+        <>
+            <button
+                onClick={() => {
+                    print.current.print()
+                }}
+            >
+                点击打印
+            </button>
+            <Print
+                print={print}
+            >
+                2131332
+            </Print>
+        </>
+    );
+};
+```
+
+<br />
+
+| 属性             | 说明                   | 类型                  | 默认值 |
+| ---------------- | ---------------------- | --------------------- | ------ |
+| width            | 表格的宽度             | `number`              | -      |
+| height           | 表格的高度信息         | `number`              | -      |
+| rows             | 表格的数据             | `T[]`                 | -      |
+| columns          | 列信息                 | `Column<T>[]`         | -      |
+| rowSelection     | 选择框的配置项         | `RowSelectType`       | -      |
+| onRowClick       | 表格单击行触发的事件   | `(row: T) => void`    | -      |
+| onRowDoubleClick | 表格双击行触发的事件   | `row: T) => void`     | -      |
+| onChange         | 改变表格数据触发的事件 | `(rows: T[]) => void` | -      |
---
+++ src/print.tsx
@@ -0,0 +1,69 @@
+import React, { FC, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import html2canvas from 'html2canvas';
+
+interface PrintInstance {
+    print: () => Promise<void>
+}
+
+interface PrintProps {
+    print: React.MutableRefObject<PrintInstance>
+}
+
+const Print: FC<PrintProps> = ({
+    print,
+    children
+}) => {
+
+    const [srcDoc, setSrcDoc] = useState<string>('')
+
+    const divRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        print.current = {
+            print: async () => {
+                if (divRef.current) {
+                    const canvas = await html2canvas(divRef.current)
+                    const base64Url = canvas.toDataURL("image/png")
+                    setSrcDoc(`
+                        <!DOCTYPE html>
+                        <html lang="zh-CN">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title></title>
+                                <style type="text/css">
+                                    @page { size: auto;  margin: 0mm; }
+                                </style>
+                                <script>
+                                    window.print()
+                                </script>
+                            </head>
+                            <body>
+                                <img src='${base64Url}'></img>
+                            </body>
+                        </html>
+                    `)
+                }
+            }
+        }
+    }, [])
+
+    return (
+        <>
+            <iframe
+                style={{
+                    display: 'none'
+                }}
+                srcDoc={srcDoc}
+            />
+            <div
+                ref={divRef}
+            >
+                {children}
+            </div>
+        </>
+    );
+};
+
+export default Print;
